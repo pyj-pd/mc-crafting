bb8ba2cf838f30c2b3b44cbaffea03aa07fd763a
unknown <123401626+pyj-pd@users.noreply.github.com>
2023-02-01 15:07:30 +0900

    Changed names on files.

--- value/metadata.ts
+++ value/metadata.ts
@@ -1,6 +1,6 @@
 export const metadata = {
   description: 'Search for crafting recipes of Minecraft items.',
-  'og:title': 'Minecraft Crafting Recipe',
+  'og:title': 'MC Crafting',
   'og:description':
     'Search for crafting recipes of Minecraft items. We already got latest data.',
 }
